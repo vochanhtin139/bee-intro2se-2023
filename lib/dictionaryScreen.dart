7064dfab759ee3a93316561565fe7d2a8cb987c0
Võ Chánh Tín <vochanhtin139@Tins-MacBook-Air.local>
2023-06-14 00:05:19 +0700

    add search bar in dictionary screen

--- lib/dictionaryScreen.dart
+++ lib/dictionaryScreen.dart
@@ -1,18 +1,110 @@
 import 'package:flutter/material.dart';
 
-class dictionaryScreen extends StatelessWidget {
+class dictionaryScreen extends StatefulWidget {
   const dictionaryScreen({super.key});
 
   @override
+  State<StatefulWidget> createState() {
+    return _homeScreenState();
+  }
+
+}
+
+class _homeScreenState extends State<dictionaryScreen> {
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Text('Dictionary Screen'),
-        SearchBar()
-      ],
+      appBar: AppBar(
+        title: const Text('My Personal Journal'),
+        automaticallyImplyLeading: false,
+        centerTitle: true,
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        actions: [
+          IconButton(
+            onPressed: () {
+              showSearch(
+                context: context,
+                delegate: customSearchKeyWordDictionary(),
+              );
+            },
+            icon: const Icon(Icons.search)
+          )
+        ],
       ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Center(
+            child: Text('Hi'),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class customSearchKeyWordDictionary extends SearchDelegate {
+  List<String> keywordDictionary = [
+    'hello', 'bonjour', 'hola', 'halo', 'hi'
+  ];
+
+  @override
+  List<Widget>? buildActions(BuildContext context) {
+    return [
+      IconButton(
+        onPressed: () {
+          query = '';
+        }, 
+        icon: const Icon(Icons.clear)
+      )
+    ];
+  }
+
+  @override
+  Widget? buildLeading(BuildContext context) {
+    return IconButton(
+      onPressed: () {
+        close(context, null);
+      },
+      icon: Icon(Icons.arrow_back)
+    );
+  }
+
+  @override
+  Widget buildResults(BuildContext context) {
+    List<String> matchQuery = [];
+
+    for (var item in keywordDictionary) {
+      if (item.toLowerCase().trim().contains(query.toLowerCase().trim())) {
+        matchQuery.add(item);
+      }
+    }
+
+    return ListView.builder(
+      itemCount: matchQuery.length,
+      itemBuilder: (context, index) {
+        var result = matchQuery[index];
+        return ListTile(title: Text(result));
+      },
+    );
+  }
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    List<String> matchQuery = [];
+
+    for (var item in keywordDictionary) {
+      if (item.toLowerCase().trim().contains(query.toLowerCase().trim())) {
+        matchQuery.add(item);
+      }
+    }
+
+    return ListView.builder(
+      itemCount: matchQuery.length,
+      itemBuilder: (context, index) {
+        var result = matchQuery[index];
+        return ListTile(title: Text(result));
+      },
     );
   }
 
